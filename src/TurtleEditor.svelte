001cc2d40787ed7d1ce5e8a5cff5c06064ac8e30
Erik Larsson <erik.larsson@schibsted.com>
2020-09-22 14:43:53 +0200

    Exploring the existing tokenizer, to see if it can be hijacked

--- src/TurtleEditor.svelte
+++ src/TurtleEditor.svelte
@@ -34,6 +34,27 @@
       editor.session.on('change', function(delta) {
       // delta.start, delta.end, delta.lines, delta.action
         // console.log(delta);
+
+        //console.log('getState', editor.session.getState(delta.start.row-1)); // ^^ Always outputs 'start'??
+
+        // console.log(editor.session.getTokenAt(delta.start.row, delta.start.column));
+        // {type: "string.quoted.double.turtle", value: ""Spidermean"", index: 3, start: 14}
+        // ^^ index = zero-based index of tokens in the current row
+
+        // console.log(editor.session.getTokens(delta.start.row));
+// 0: {type: "text", value: "    "}
+// 1: {type: "entity.name.other.qname.turtle", value: "foaf:name"}
+// 2: {type: "text", value: " "}
+// 3: {type: "string.quoted.double.turtle", value: ""Spidermean"", index: 3, start: 14}
+// 4: {type: "keyword.operator.punctuation.turtle", value: ","}
+// 5: {type: "text", value: " "}
+// 6: {type: "string.quoted.double.turtle", value: ""Человек-паук""}
+// 7: {type: "keyword.operator.language.suffix.turtle", value: "@"}
+// 8: {type: "constant.language.suffix.turtle", value: "ru"}
+// 9: {type: "text", value: " "}
+// 10: {type: "keyword.operator.punctuation.turtle", value: "."}
+// ^^ Only looks at current row, e.g. does not include parts of the statement from earlier rows. Also, seems to be somewhat low-level and not include "predicate" or "object" for example.
+
         if (delta.action === 'insert' && delta.lines[0] === 'x') {
           editor.undo();
           editor.insert(' .\n');
@@ -48,6 +69,11 @@
 
       // editor.session.selection.on('changeCursor', function(e) {
       //   console.log(e);
+      // });
+
+      // editor.session.on("tokenizerUpdate", function(e) {
+      //   console.log('tokenizerUpdate', e);
+      //   // e.data.first + e.data.last -- maybe which rows are tokenized?
       // });
     }
   }
